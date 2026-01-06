ba618cb9e369ce769ab6b6467ed1264f7c22186a
Leon <leondo123@gmail.com>
2026-01-05 19:45:34 -0600

    toggle notifications

--- packages/snap/src/index.tsx
+++ packages/snap/src/index.tsx
@@ -1,5 +1,35 @@
-import type { OnCronjobHandler, OnInstallHandler } from '@metamask/snaps-sdk';
-import { Text } from '@metamask/snaps-sdk/jsx';
+import type {
+  OnHomePageHandler,
+  OnCronjobHandler,
+  OnInstallHandler,
+} from '@metamask/snaps-sdk';
+// https://github.com/MetaMask/snaps/blob/main/packages/snaps-sdk/src/jsx/components/index.ts
+import {
+  Box,
+  Heading,
+  Text,
+  Checkbox,
+  Row,
+  Divider,
+} from '@metamask/snaps-sdk/jsx';
+
+export const onHomePage: OnHomePageHandler = async () => {
+  return {
+    content: (
+      <Box>
+        <Heading>PEPE Unchained Notifications</Heading>
+        <Divider />
+        {/* <Text>Notifications</Text> */}
+        <Row label="Important News">
+          <Checkbox name="fooo" label="" checked={true} variant="toggle" />
+        </Row>
+        <Row label="Pumppad.gg">
+          <Checkbox name="bar" label="" checked={false} variant="toggle" />
+        </Row>
+      </Box>
+    ),
+  };
+};
 
 // https://docs.metamask.io/snaps/reference/entry-points#oninstall
 export const onInstall: OnInstallHandler = async ({}) => {
